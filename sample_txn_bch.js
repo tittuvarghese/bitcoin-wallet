291a1684deea0e3c77df80bb575a9039bf8276e4
tittuvarghese <2311294+tittuvarghese@users.noreply.github.com>
2019-03-26 18:53:11 +0530

    Bch fix

--- sample_txn_bch.js
+++ sample_txn_bch.js
@@ -1,5 +1,6 @@
+const request = require('request');
 const explorers = require('bitcore-explorers')
-const insight = new explorers.Insight('https://test-bch-insight.bitpay.com')
+// const insight = new explorers.Insight('https://test-bch-insight.bitpay.com')
 const bch = require('bitcore-lib-cash')
 const satoshiConverter = 100000000;
 
@@ -9,20 +10,21 @@ let receiverAddress = "bchtest:qps60h780gvjcn80gn6d576w2nt80k47tv0wa2e6j6"
 
 let txnAmount = 0.0009 * satoshiConverter // 0.001 BCH
 
-insight.getUnspentUtxos(senderAddress, function (error, utxos) {
-  if (error) {
-    console.error(error)
-    return
+
+request('https://test-bch-insight.bitpay.com/api/addrs/'+senderAddress+'/utxo', { json: true }, (err, res, utxos) => {
+  if(err) {
+    console.log("unable to fetch utx.")
+  } else if(res.statusCode == 500) {
+    console.log("No UTXs found")
+  } else {
+
+    const transaction = new bch.Transaction()
+        .from(utxos)
+        .to(receiverAddress, txnAmount)
+        .change(senderAddress)
+        .sign(privateKey)
+
+      console.log(transaction.toString())
+
   }
-
-  //console.log(utxos)
-
-
-  const transaction = new bch.Transaction()
-    .from(utxos)
-    .to(receiverAddress, txnAmount)
-    .change(senderAddress)
-    .sign(privateKey)
-
-  console.log(transaction.toString())
-})
+});
